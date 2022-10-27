89ef6f7d3ab3dc0480404036e391773c63e2d3b6
linmn <linmn@gddxit.com>
2022-10-27 15:10:07 +0800

    修改异步请求数据格式返回处理，赋值

--- src/components/Translator/Template/Form.vue
+++ src/components/Translator/Template/Form.vue
@@ -1,6 +1,18 @@
+<template>
+  <AnsoDataform
+    ref="form"
+    id="customForm"
+    v-model="formData"
+    v-bind="this.formAttrs"
+    :formFields="this.formFieds"
+    :buttonList="this.formButtons"
+    @onChange="this.onFormChange"
+  ></AnsoDataform>
+</template>
 <script>
 import { merge, keyBy } from 'lodash'
 import { formatFormRules } from '@/utils/format.js'
+// import { useEval } from '@/utils/request.js'
 export default {
   name: 'FormTemp',
   props: {
@@ -39,7 +51,12 @@ export default {
       })
     },
     formButtons () {
-      return this.config?.form.buttons
+      return this.config?.form.buttons.map(btn => {
+        if (btn.name === 'reset') { btn.func = this.onFormReset }
+        if (btn.name === 'cancel') { btn.func = this.onFormCancel }
+        if (btn.name === 'submit') { btn.func = () => this.onFormSubmit(btn) }
+        return btn
+      })
     },
     formAttrs () {
       const { attrs } = this.config?.form || {}
@@ -63,8 +80,28 @@ export default {
     onFormChange () {
       console.info(arguments)
     },
+    // 内置的按钮重置函数
     onFormReset () {
       this.$refs.form.resetForm()
+    },
+    // 内置的按钮取消函数
+    onFormCancel () {},
+    // 内置的按钮提交事件
+    onFormSubmit (btn) {
+      console.log('on submit', btn)
+      if (btn.validate) {
+        this.$refs.form.$refs.dataform.validate(valid => {
+          if (valid && btn.funcRemote) {
+            this.$require(btn.funcRemote).then(res => {
+              console.info('on after submit', res)
+            })
+          }
+        })
+      } else if (btn.funcRemote) {
+        this.$require(btn.funcRemote).then(res => {
+          console.info('on after submit', res)
+        })
+      }
     },
     onFormValidate () {
       this.$refs.form.validate()
@@ -80,7 +117,14 @@ export default {
     // 设置默认值
     setDefaultValue () {},
     // 发起异步请求
-    getRemoteResource () {},
+    getRemoteResource () {
+      const { actions } = this.config?.form || {}
+      if (actions?.immediateRemoteRequire) {
+        this.$require(actions.immediateRemoteRequire).then(res => {
+          this.formData = res || {}
+        })
+      }
+    },
     // 获取relation
     getRelationFields () {
       return this.formFieds.reduce((res, field) => {
@@ -99,11 +143,10 @@ export default {
         const { fByKey, fByRemote } = this.getRelationFields()
 
         if (gbResource && fByKey?.length) {
+          // 根据字段配置relation字典key，赋值
           this.$require(gbResource)
             .then(res => {
-              // console.info('请求relation', res)
               if (res) {
-                const result = res?.data?.data ? keyBy(res?.data?.data, 'name') : {}
                 // console.info(res?.data?.data?.[0]?.data)
                 fByKey.forEach(f => {
                   // f.options = result[f.optionRelationKey]
@@ -111,8 +154,9 @@ export default {
                     // 测试数据
                     // console.info(res?.data?.data?.[0]?.data)
                     // TODO 对数据结构的取值层级优化成可配置的
-                    this.$set(f, 'options', res?.data?.data?.[0]?.data)
+                    this.$set(f, 'options', res?.data?.[0]?.data)
                   } else {
+                    const result = res?.data ? keyBy(res?.data, 'name') : {}
                     this.$set(f, 'options', result[f.optionRelationKey])
                   }
                 })
@@ -120,12 +164,13 @@ export default {
             })
         }
         if (fByRemote?.length) {
+          // 根据每个字段配置了动态异步获取relation，赋值
           Promise.all(fByRemote.map(f => this.$require(f.optionsAsyncFunc)
             .then(res => { return res || {} })))
             .then((results) => {
               results.forEach((r, i) => {
                 // TODO 对数据结构的取值层级优化成可配置的
-                this.$set(fByRemote[i], 'options', r?.data?.data || [])
+                this.$set(fByRemote[i], 'options', r?.data || [])
               })
             })
         }
@@ -140,20 +185,21 @@ export default {
     // this.onMounted()
     this.onClearValidate()
     this.getRemoteResource()
-  },
-  render (h) {
-    return (
-      <div>
-        <AnsoDataform
-          ref="form"
-          id="customForm"
-          formFields={this.formFieds}
-          buttonList={this.formButtons}
-          props={this.formAttrs}
-          onChange={this.onFormChange}
-        ></AnsoDataform>
-      </div>
-    )
+  // },
+  // render (h) {
+  //   return (
+  //     <div>
+  //       <AnsoDataform
+  //         ref="form"
+  //         id="customForm"
+  //         v-model="formData"
+  //         formFields={this.formFieds}
+  //         buttonList={this.formButtons}
+  //         props={this.formAttrs}
+  //         onChange={this.onFormChange}
+  //       ></AnsoDataform>
+  //     </div>
+  //   )
   }
 }
 </script>
