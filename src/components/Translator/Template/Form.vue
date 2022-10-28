f0c67443ed5726dfaeabfa2bfbbd762bfa99444b
linmn <linmn@gddxit.com>
2022-10-28 14:43:50 +0800

    表单模板渲染内容修改

--- src/components/Translator/Template/Form.vue
+++ src/components/Translator/Template/Form.vue
@@ -5,16 +5,20 @@
     v-model="formData"
     v-bind="this.formAttrs"
     :formFields="this.formFieds"
-    :buttonList="this.formButtons"
+    :buttonList="this.actButtons"
     @onChange="this.onFormChange"
   ></AnsoDataform>
 </template>
 <script>
-import { merge, keyBy } from 'lodash'
+import { merge, isEqual, cloneDeep } from 'lodash'
 import { formatFormRules } from '@/utils/format.js'
+// import button from '../mixins/button'
+import relation from '../mixins/relation'
+import { MessageBox } from 'element-ui'
 // import { useEval } from '@/utils/request.js'
 export default {
   name: 'FormTemp',
+  mixins: [relation],
   props: {
     config: {
       type: Object,
@@ -27,6 +31,7 @@ export default {
   },
   data () {
     return {
+      formDataTemp: {},
       formData: {},
       relations: {}
     }
@@ -50,14 +55,6 @@ export default {
         }
       })
     },
-    formButtons () {
-      return this.config?.form.buttons.map(btn => {
-        if (btn.name === 'reset') { btn.func = this.onFormReset }
-        if (btn.name === 'cancel') { btn.func = this.onFormCancel }
-        if (btn.name === 'submit') { btn.func = () => this.onFormSubmit(btn) }
-        return btn
-      })
-    },
     formAttrs () {
       const { attrs } = this.config?.form || {}
       return {
@@ -67,41 +64,30 @@ export default {
         disabled: attrs.readOnly
       }
     },
-    // 需要发起异步选项的字段
-    remoteOptions () {
-      return this.formItems.filter(field => field.optionType === 'optionsAsyncFunc')
-    },
     // 需要发起异步搜索的字段
     remoteSearch () {
       return this.formItems.filter(field => field.filterAbleType === 'filterAbleAsyncFunc')
+    },
+    hasChangeData () {
+      return !isEqual(this.formData, this.formDataTemp)
+    },
+    actButtons () {
+      const buttons = cloneDeep(this.config?.form.buttons)
+      return buttons.map(btn => {
+        if (btn.name === 'reset') { btn.func = () => this.onFormReset(btn) }
+        if (btn.name === 'cancel') { btn.func = () => this.onFormCancel(btn) }
+        if (btn.name === 'submit') { btn.func = () => this.onFormSubmit(btn) }
+        if (this.config?.form?.attrs?.readOnly) {
+          btn.disabled = true
+        }
+        return btn
+      })
     }
   },
   methods: {
     onFormChange () {
-      console.info(arguments)
-    },
-    // 内置的按钮重置函数
-    onFormReset () {
-      this.$refs.form.resetForm()
-    },
-    // 内置的按钮取消函数
-    onFormCancel () {},
-    // 内置的按钮提交事件
-    onFormSubmit (btn) {
-      console.log('on submit', btn)
-      if (btn.validate) {
-        this.$refs.form.$refs.dataform.validate(valid => {
-          if (valid && btn.funcRemote) {
-            this.$require(btn.funcRemote).then(res => {
-              console.info('on after submit', res)
-            })
-          }
-        })
-      } else if (btn.funcRemote) {
-        this.$require(btn.funcRemote).then(res => {
-          console.info('on after submit', res)
-        })
-      }
+      // console.info(arguments)
+      this.$emit('')
     },
     onFormValidate () {
       this.$refs.form.validate()
@@ -109,10 +95,6 @@ export default {
     onClearValidate () {
       this.$refs.form.$refs.dataform.clearValidate()
     },
-    // onMounted () {
-    //   this.onClearValidate()
-    //   this.getRemoteResource()
-    // },
     onDestory () {},
     // 设置默认值
     setDefaultValue () {},
@@ -121,59 +103,102 @@ export default {
       const { actions } = this.config?.form || {}
       if (actions?.immediateRemoteRequire) {
         this.$require(actions.immediateRemoteRequire).then(res => {
+          this.formDataTemp = res || {}
           this.formData = res || {}
         })
       }
     },
-    // 获取relation
-    getRelationFields () {
-      return this.formFieds.reduce((res, field) => {
-        const { optionType } = field?.form || {}
-        if (optionType === 'optionRelationKey') {
-          res.fByKey.push(field?.form)
-        } else if (optionType === 'optionsAsyncFunc') {
-          res.fByRemote.push(field?.form)
-        }
-        return res
-      }, { fByKey: [], fByRemote: [] })
+    // 内置的按钮重置函数
+    doReset () {
+      this.$refs.form.resetForm()
     },
-    getRelation () {
-      const { getRelationImmediate, relationResource: gbResource } = this.config?.form?.actions || {}
-      if (getRelationImmediate) {
-        const { fByKey, fByRemote } = this.getRelationFields()
-
-        if (gbResource && fByKey?.length) {
-          // 根据字段配置relation字典key，赋值
-          this.$require(gbResource)
-            .then(res => {
-              if (res) {
-                // console.info(res?.data?.data?.[0]?.data)
-                fByKey.forEach(f => {
-                  // f.options = result[f.optionRelationKey]
-                  if (this.isTest) {
-                    // 测试数据
-                    // console.info(res?.data?.data?.[0]?.data)
-                    // TODO 对数据结构的取值层级优化成可配置的
-                    this.$set(f, 'options', res?.data?.[0]?.data)
-                  } else {
-                    const result = res?.data ? keyBy(res?.data, 'name') : {}
-                    this.$set(f, 'options', result[f.optionRelationKey])
-                  }
-                })
-              }
-            })
+    onFormReset (btn) {
+      //  && this.hasChangeData
+      if (btn.tipBeforeAction) {
+        MessageBox.confirm(
+          '检测到未保存的内容，是否确定当前操作？',
+          `${btn.label}提醒`,
+          {
+            distinguishCancelAndClose: true,
+            confirmButtonText: '确定',
+            cancelButtonText: '取消'
+          })
+          .then(confirm => {
+            if (confirm) {
+              this.doReset()
+            }
+          })
+      } else {
+        this.doReset()
+      }
+    },
+    doCancel (btn) {
+      this.$refs.form.resetForm()
+      try {
+        if (btn.returnBack) {
+          if (this.$router) {
+            this.$router.go(-1)
+          } else {
+            window.history.go(-1)
+          }
+        } else if (btn.closeDialog) {
+          this.$emit('onCloseDialog')
         }
-        if (fByRemote?.length) {
-          // 根据每个字段配置了动态异步获取relation，赋值
-          Promise.all(fByRemote.map(f => this.$require(f.optionsAsyncFunc)
-            .then(res => { return res || {} })))
-            .then((results) => {
-              results.forEach((r, i) => {
-                // TODO 对数据结构的取值层级优化成可配置的
-                this.$set(fByRemote[i], 'options', r?.data || [])
-              })
-            })
+      } catch (err) {
+        console.warn('取消功能异常:', err)
+      }
+    },
+    // 内置的按钮取消函数
+    onFormCancel (btn) {
+      if (btn.tipBeforeAction) {
+        MessageBox.confirm(
+          '检测到未保存的内容，是否确定当前操作？',
+          `${btn.label}提醒`,
+          {
+            distinguishCancelAndClose: true,
+            confirmButtonText: '确定',
+            cancelButtonText: '取消'
+          })
+          .then(confirm => {
+            if (confirm) {
+              this.doCancel()
+            }
+          })
+      } else {
+        this.doCancel(btn)
+      }
+    },
+    formatSubmitParams (params = []) {
+      const formParams = Object.entries(this.formData).map(([name, value]) => {
+        return {
+          key: name,
+          value,
+          varType: 'const' // 无需动态取值
         }
+      })
+      // TODO 当body中变量类型value存在`${row.varType}|${row.value}`这个格式 需要转换成动态取值
+      return [].concat(params, formParams)
+    },
+    // 内置的按钮提交事件
+    onFormSubmit (btn) {
+      console.log('on submit', btn)
+      if (btn.validate) {
+        this.$refs.form.$refs.dataform.validate(valid => {
+          if (valid && btn.funcRemote) {
+            this.$require({
+              ...btn.funcRemote,
+              body: this.formatSubmitParams(btn.body)
+            }).then(res => {
+              // console.info('on after submit', res)
+              this.$emit('onAfterSubmit', res)
+            })
+          }
+        })
+      } else if (btn.funcRemote) {
+        this.$require(btn.funcRemote).then(res => {
+          // console.info('on after submit', res)
+          this.$emit('onAfterSubmit', res)
+        })
       }
     }
   },
@@ -194,7 +219,7 @@ export default {
   //         id="customForm"
   //         v-model="formData"
   //         formFields={this.formFieds}
-  //         buttonList={this.formButtons}
+  //         buttonList={this.actButtons}
   //         props={this.formAttrs}
   //         onChange={this.onFormChange}
   //       ></AnsoDataform>
