fdfa1e15f9f21b0222ec9e81619c1a996c5faefc
linmn <linmn@ansosz.cn>
2023-04-07 18:19:33 +0800

    调整全局环境服务布局

---
+++ src/components/ConfigSetter/WebserverSetter/ServerContent.vue
@@ -0,0 +1,54 @@
+<template lang='pug'>
+.right-custom-data.d-flex-1.p-r-8
+  //- 环境配置
+  el-form(ref="apiForm", :model="computedData", label-position="left")
+    el-form-item(label="环境名称", prop="label")
+      el-input(v-model="computedData.label")
+    el-form-item(label="服务配置（前置URL）", prop="urls")
+      //- el-input(v-model="computedData.group", placeholder="请输入分组标题名称")
+      form-list(
+        v-model="computedData.urls"
+        style="width: 90%"
+        :columnProps="urlColumnProps"
+        :draggable="false")
+    el-form-item(label="环境变量", prop="vars")
+    //- el-form-item(label="头部参数", prop="vars")
+    //- el-form-item(label="头部参数", prop="vars")
+</template>
+
+<script>
+export default {
+  name: 'ServerContent',
+  props: {
+    value: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  components: {},
+  data () {
+    return {
+      urlColumnProps: [
+        { label: '服务名', prop: 'title' },
+        { label: '前置URL', prop: 'url' }
+      ]
+    }
+  },
+  computed: {
+    computedData: {
+      get () {
+        return this.value
+      },
+      set (obj) {
+        this.$emit('input', obj)
+      }
+    }
+  },
+  watch: {},
+  methods: {},
+  mounted () {}
+}
+</script>
+
+<style scoped lang='sass'>
+</style>
--- src/components/ConfigSetter/WebserverSetter/index.vue
+++ src/components/ConfigSetter/WebserverSetter/index.vue
@@ -18,38 +18,11 @@ el-dialog.webserver-setter-dialog(
       //-   el-button(icon="el-icon-plus", @click="addApi") 新增数据源
     .bottom-wrap.d-flex-row-between
       //- 左
-      .left-wrap.d-flex-column
-        .left-wrap-tool
-          .d-flex-row-between.d-flex-v-center
-            .d-flex-1.text.color-secondary 环境
-              //- el-checkbox(v-model="allSelectedVisable") 查看画布内已选的数据源
-            el-button-group
-              el-button(icon="el-icon-plus", title="新增环境", @click="addService")
-        .left-wrap-list.d-flex-1.m-t-8
-          //- 左边环境列表
-          .left-item.list-row.d-flex-row-between.align-items-center.hover-change-bgColor(
-            v-for="(service, index) in serviceList"
-            :key="service.name"
-            :id="service.name"
-            :is-active="serviceData.name === service.name"
-            @click="handleService(service)")
-            .text-left {{service.label}}
-            .text-right(v-show="service.delAble")
-              i.iconfont.el-icon-delete(@click="onDeleteEnv(index)", title="删除环境")
+      ServerList(v-model="serviceData")
       //- 右
       .right-wrap.d-flex-column.d-flex-1
-        .right-custom-data.d-flex-1.p-r-8
-          //- 环境配置
-          el-form(ref="apiForm", :model="serviceData", label-position="left")
-            el-form-item(label="环境名称", prop="label")
-              el-input(v-model="serviceData.label")
-            el-form-item(label="服务配置（前置URL）", prop="urls")
-              //- el-input(v-model="serviceData.group", placeholder="请输入分组标题名称")
-              form-list(style="width: 90%", :columnProps="urlColumnProps", v-model="serviceData.urls", :draggable="false")
-            el-form-item(label="环境变量", prop="vars")
-            //- el-form-item(label="头部参数", prop="vars")
-            //- el-form-item(label="头部参数", prop="vars")
-
+        ServerContent(v-model="serviceData")
+        //- 下
         .right-bottom__fixed.d-flex-row-between
           el-button-group
             //- el-button(@click="testLink") 测试链接
@@ -58,12 +31,14 @@ el-dialog.webserver-setter-dialog(
             el-button-group
               el-button(@click="onClose") 关闭
               el-button(@click="onSave") 保存修改
-              el-button(type="primary", title="保存至当前，不影响全局", @click="chooseChange") 确认切换环境
+              el-button(type="primary", title="保存至当前，不影响全局", @click="chooseChange") 应用所有画布
 </template>
 
 <script>
 /* 环境配置 */
-import { EnvModel, defaultEnvConf } from '@/model/service'
+import { defaultEnvConf } from '@/model/service'
+import ServerList from './ServerList.vue'
+import ServerContent from './ServerContent.vue'
 export default {
   name: 'WebserverSetter',
   props: {
@@ -72,14 +47,14 @@ export default {
       default: false
     }
   },
+  components: {
+    ServerContent,
+    ServerList
+  },
   data () {
     return {
-      serviceList: defaultEnvConf, // 环境列表，默认取本地环境, 只能删除自定义的环境
-      serviceData: {}, // 环境配置数据
-      urlColumnProps: [
-        { label: '服务名', prop: 'title' },
-        { label: '前置URL', prop: 'url' }
-      ]
+      // serviceList: defaultEnvConf, // 环境列表，默认取本地环境, 只能删除自定义的环境
+      serviceData: {} // 环境配置数据
     }
   },
   computed: {
@@ -97,37 +72,21 @@ export default {
     onSave () {},
     // 确定却换
     chooseChange () {},
-    // 增加环境
-    addService () {
-      const hasEmpty = this.serviceList.find(item => !item.label)
-      // console.info('不存在空白环境:', hasEmpty)
-      if (!hasEmpty) {
-        // 不存在空白环境才可添加
-        // const evnKey = `env_${ New Date().getTime() }`
-        this.serviceList.push(new EnvModel({
-          name: 'env_' + new Date().getTime(),
-          delAble: true
-        }))
-        this.$nextTick(() => {
-          this.serviceData = this.serviceList[this.serviceList.length - 1]
-        })
-      } else {
-        document.getElementById(hasEmpty.name).focus()
-      }
-    },
-    onDeleteEnv (index) {
-      this.$delete(this.serviceList, index)
-    },
-    handleService (env) {
-      this.serviceData = env
-    },
+    // onSelectServer (env) {
+    //   this.serviceData = env
+    // },
     onClose () {
       this.dialogVisabled = false
       this.$emit('onClose')
+    },
+    init () {
+      this.serviceData = defaultEnvConf?.[0]
     }
 
   },
-  mounted () {}
+  mounted () {
+    this.init()
+  }
 }
 </script>
 
@@ -143,24 +102,11 @@ export default {
   .bottom-wrap
     overflow-y: hidden
     height: 100%
-.left-wrap
-  width: 20%
-  overflow-y: auto
-  background: rgb(243 244 249 / 30%)
-  .list-row
-    // border: 1px solid $--border-color-base
-    padding: 8px
-    cursor: pointer
-    position: relative
-    // &:hover
-    //   background: $--bgcolor-secondary
-    & + .list-row
-      margin-top: 8px
-      // border-top: 1px solid $--border-color-base
-    &[is-active], &:hover
-      background: $--bgcolor-secondary
-      border-radius: 4px
-      color: $--color-primary
+
+.left-wrap, .right-wrap
+  // border: 1px solid $--border-color-base
+  padding: 8px
+
 .right-wrap
   flex: 1 0 auto
   // max-width: 70%
@@ -177,10 +123,6 @@ export default {
     background: #fff
     padding-top: 8px
     box-shadow: 0px -3px 2px rgb(220 223 230 / 30%)
-
-.left-wrap, .right-wrap
-  // border: 1px solid $--border-color-base
-  padding: 8px
 
 .left-top__tip
   span + span
